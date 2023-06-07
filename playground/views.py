1676306ef18b596b9d775abd2ed11c3f26c9b7df
Gerry Laas <gerry@bl4ckcod3.com>
2023-06-07 09:00:06 +0200

    Created and mapped views
    Created and mapped views to url path

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here.Request Handler
+
+
+def say_hello(request):
+    return render(request, 'hello.html')
