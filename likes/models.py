73fa5356f33f872f18f48f768d3bf280ff384fd3
Gerry Laas <gerry@bl4ckcod3.com>
2023-06-07 12:37:04 +0200

    Migrations
    Setup migrations

--- likes/models.py
+++ likes/models.py
@@ -1,16 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-# from store.models import Product -> store app is dependant an tag app.
 
-
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-
-
-class TaggedItem(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+class LikedItems(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
-    # Poor way > product = models.ForeignKey(Product)
--- storefront/settings.py
+++ storefront/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'store',
     'tags',
+    'likes',
 ]
 
 MIDDLEWARE = [
@@ -86,8 +87,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'storefront',
+        'USER': 'postgres',
+        'PASSWORD': 'A1v}#[QLW]O!UeUH',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
